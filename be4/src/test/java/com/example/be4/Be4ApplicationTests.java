20ba1802eb2a5bd6a88531c5196d14c00ade3e26
devJeenee <eui415566@gmail.com>
2025-03-10 10:21:31 +0900

    first push

---
+++ be4/src/test/java/com/example/be4/Be4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
